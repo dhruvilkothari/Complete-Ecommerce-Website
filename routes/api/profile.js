2a39719d2d60de1bc344f2441f8c96246865548e
Dhruvil <kotharidhruvil3@gmail.com>
2021-07-29 08:17:11 +0530

    Almost completed the profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -174,4 +174,128 @@ router.delete("/", auth, async (req, res) => {
 // @des add Profile profule experience
 // @acess Private
 
+router.put(
+  "/experience",
+  [
+    auth,
+    [
+      check("title").not().isEmpty(),
+      // check("company", "Company Field is required").isEmpty(),
+      check("title", "Title is required").not().isEmpty(),
+      check("from", "From Date is required").not().isEmpty(),
+    ],
+  ],
+  async (req, res, next) => {
+    {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(404).json({ errors: errors.array() });
+      }
+
+      const { title, company, location, from, current, description } = req.body;
+      const newExp = {
+        title,
+        company,
+        location,
+        from,
+        current,
+        description,
+      };
+
+      try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExp);
+        await profile.save();
+        res.json(profile);
+      } catch (err) {
+        console.log(err.message);
+        res.status(500).json({ msg: err.message });
+      }
+    }
+  }
+);
+
+// @route put api/profile/experience
+// @des add Profile profule experience
+// @acess Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    profile.experience = profile.experience.filter((item) => {
+      item.id != req.params.exp_id;
+    });
+    await profile.save();
+    res.send(profile);
+  } catch (err) {}
+});
+
+// @route put api/profile/eductaion
+// @des add profile Education
+// @acess Private
+
+router.put(
+  "/education",
+  [
+    auth,
+    [
+      check("school", "School is required").not().isEmpty(),
+      // check("company", "Company Field is required").isEmpty(),
+      check("degree", "Degree is required").not().isEmpty(),
+      check("from", "From Date is required").not().isEmpty(),
+      // check("fieldofstudy", "Field of study Required").not().isEmpty(),
+    ],
+  ],
+  async (req, res, next) => {
+    {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        console.log(errors);
+        return res.status(404).json({ errors: errors.array() });
+      }
+
+      const { school, degree, fieldofstudy, from, to, current, description } =
+        req.body;
+      const newEdu = {
+        school,
+        degree,
+        fieldofstudy,
+        from,
+        to,
+        current,
+        description,
+      };
+
+      try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.education.unshift(newEdu);
+        await profile.save();
+        res.json(profile);
+      } catch (err) {
+        console.log(err);
+        res.status(500).json({ msg: err.message });
+      }
+    }
+  }
+);
+
+// @route put api/profile/education
+// @des add Profile profule education
+// @acess Private
+
+router.delete("/education/:edu_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    profile.education = profile.education.filter((item) => {
+      item.id != req.params.edu_id;
+    });
+    await profile.save();
+    res.send(profile);
+  } catch (err) {}
+});
+
 module.exports = router;
